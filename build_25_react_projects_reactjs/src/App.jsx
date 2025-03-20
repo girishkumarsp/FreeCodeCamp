e316642c04d814881a67d1d4eaeda286fa590922
girishkumarsp <52626613+girishkumarsp@users.noreply.github.com>
2025-03-20 15:02:25 +0000

    load-more-data

--- build_25_react_projects_reactjs/src/App.jsx
+++ build_25_react_projects_reactjs/src/App.jsx
@@ -6,6 +6,7 @@ import Accordian from "./components/accordian";
 import RandomColor from "./components/random-color";
 import StarRating from "./components/star-rating";
 import ImageSlider from "./components/image-slider";
+import LoadMoreData from "./components/load-more-data";
 
 function App() {
   return (
@@ -20,7 +21,10 @@ function App() {
       {/* <StarRating noOfStars={10}/> */}
 
       {/* image slider */}
-      <ImageSlider url={"https://picsum.photos/v2/list"} limit={10} page={1} />
+      {/* <ImageSlider url={"https://picsum.photos/v2/list"} limit={10} page={1} /> */}
+
+      {/* LoadMoreData */}
+      <LoadMoreData/>
     </>
   );
 }
